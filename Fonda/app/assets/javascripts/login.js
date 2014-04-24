fce47784ea313b3de3c2f39afbba8ce631d1a7d3
Joshua Aranda <aranda.moreno.joshua@gmail.com>
2014-04-24 11:33:15 -0700

    Validate user

--- Fonda/app/assets/javascripts/login.js
+++ Fonda/app/assets/javascripts/login.js
@@ -52,23 +52,39 @@ $('#add-food').validate({
 			required: "Please provide a description"
 		}
 	},
-/*	submitHandler: function(form) {
-		$(form).ajaxSubmit({
-			dataType: 'JSON',
-			beforeSend: function() {
-				console.log('sending');
-			},
-			error: function() {
-				console.log('an error was found');
-			},
-			success: function(response) {
-				if (!response.success) return alert('Your request cannot be submitted!');
+});
 
-				location.href = document.referrer;
-			},
-			complete: function() {
-				console.log('complete!');
-			}
-		});
-	}*/
+$('#add-user').validate({
+	rules: {
+		name: {
+			required: true,
+			minlength: 2
+		},
+		last_name: {
+			required: true,
+			minlength: 2
+		},
+		username: {
+			required: true,
+			minlength: 7
+		},
+		password: {
+			required: true,
+			minlength: 7
+		},
+	},
+	messages: {
+		name: {
+			required: "Please enter a first name"
+		},
+		last_name: {
+			required: "Please enter a last name"
+		},
+		username: {
+			required: "Your username must consist of at least 2 characters"
+		},
+		password: {
+			required: "Please enter a password"
+		},
+	},
 });
