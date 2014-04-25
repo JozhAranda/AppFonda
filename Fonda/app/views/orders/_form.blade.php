fd6ab264ed780815614c2250feea9f08f2f95854
Joshua Aranda <aranda.moreno.joshua@gmail.com>
2014-04-25 15:09:19 -0700

    Update _form.blade.php

--- Fonda/app/views/orders/_form.blade.php
+++ Fonda/app/views/orders/_form.blade.php
@@ -1,26 +1,15 @@
-<!--div class="form-group">
-	{{ Form::hidden('user', Auth::user()->id, null, array('class'=>'form-control')) }}
-</div>
-<div class="form-group">
-	{{ Form::label('foods', 'Foods') }}
-	{{ Form::select('foods', $food, null, array('class'=>'form-control')) }}
-</div>
-<div class="form-group">
-	{{ Form::submit('Save', array('class' => 'btn btn-success')) }} 
-		{{ link_to('orders','Cancel', array('class'=>'btn btn-danger'))}}
-</div-->
 <h3>Food</h3>
 <?php $i = 0 ?>
 <div class="row">
 @foreach ($food as $food)
       <?php $i = $i + 1 ?>
-      <div class="col-xs-5 col-sm-2" style="margin:10px">
+      <div class="col-xs-5 col-sm-2" style="margin:10px" id="accordion">
       	<img src="{{ asset('images/foods/Jellyfish.jpg') }}" width="120" alt="" />
-		<h4>{{$food}}</h4>
-        <span class="text-muted">{{ $food }}
-        	{{ Form::checkbox('foods[]', $i, null, array('class' => 'input-xs')) }}
-        </span>
-        
+		<a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$food->id}}" data-toggle="tooltip" data-placement="left" title="Tooltip on left"><h4>{{$food->name}}</h4></a>
+        <div id="collapse{{$food->id}}" class="panel-collapse collapse out">
+	        <span class="text-muted">{{ $food->description }}</span>
+        </div>
+	    <span class="label label-default">Select here {{ Form::checkbox('foods[]', $food->id, null, array('class' => 'input-xs')) }}</span>
       </div>
 @endforeach
 </div>
